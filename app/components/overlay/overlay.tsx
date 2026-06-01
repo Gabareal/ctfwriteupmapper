e670d48f4219d29aedab0e4b43d0ef3f87d2e846
Gabriel Ng <119044507+Gabareal@users.noreply.github.com>
2026-06-01 14:42:35 +0800

    getting marked

--- app/components/overlay/overlay.tsx
+++ app/components/overlay/overlay.tsx
@@ -1,11 +1,30 @@
 import './overlay.css'
-import ReactMarkdown from "react-markdown"
+import { useState } from "react";
+import { marked } from 'marked';
+import ReactMarkdown from "react-markdown";
+// Import element definition
+import ZeroMd from 'zero-md'
+
+// Register custom element
+customElements.define('zero-md', ZeroMd)
+
+// Render anywhere
+
 
 export default function Overlay({ data }) {
+    var parsedtext = 'none'
+    if (data) {
+        const src = `app/files/${data.label}.md`
+        var text = fetch(src)
+        if (!text.ok) {
+          throw new Error(`text status: ${text.status}`);
+        }
+        const rawtext = text.text()
+        var parsedtext = marked.parse(rawtext)
+    } 
     return (
         <div id="overlay" style={{display: data ? 'block' : 'none'}}>
-            <h1>{data.label}</h1>
-            <p>{data.description}</p>
+            <zero-md src={src}></zero-md>
         </div>
     );
 }
